2cfae0005476326d6311a406a75328c21aef032e
JeisonCastro <jeison19ca@gmail.com>
2026-04-17 20:53:44 -0500

    Update widget.js

--- widget.js
+++ widget.js
@@ -55,7 +55,7 @@
 
         try {
             // USAMOS TU MISMA FUNCIÓN DE NETLIFY
-            const res = await fetch('https://TU-DOMINIO.netlify.app/.netlify/functions/chat', {
+            const res = await fetch('https://jeison.digital.netlify.app/.netlify/functions/chat', {
                 method: 'POST',
                 body: JSON.stringify({ prompt: text, agente_id: agenteId })
             });
